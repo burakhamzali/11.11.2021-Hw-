8cb52571d83a444c55a1af91df2635bd48614a29
Burak Hamzalı <67513190+burakhamzali@users.noreply.github.com>
2021-11-12 01:18:16 +0300

    Update MyList.java

--- MyList/src/MyList.java
+++ MyList/src/MyList.java
@@ -20,12 +20,50 @@ public class MyList {
 		}
 		return liste;
 	}
-	
+
 	public void list() {
 		System.out.println("Listenin Elemanları:");
 		for (String result : liste) {
 			System.out.println(result);
 		}
+	}
+
+	public void remove(String element) {
+
+		String[] tempListe = new String[liste.length];
+		for (int i = 0; i < liste.length; i++) {
+			tempListe[i] = liste[i];
+		}
+
+		liste = new String[liste.length - 1];
+		int x = 0;
+		for (int i = 0; i < liste.length + 1; i++) {
+			if (tempListe[i] == element) {
+				continue;
+			} else {
+				liste[x] = tempListe[i];
+
+			}
+			x++;
+		}
+	}
+
+	public void remove(int index) {
+		String[] tempListe = new String[liste.length];
+		for (int i = 0; i < tempListe.length; i++) {
+			tempListe[i] = liste[i];
+		}
+		liste = new String[tempListe.length - 1];
+		int x = 0;
+		for (int i = 0; i < liste.length; i++) {
+			if (i == index) {
+				continue;
+			} else {
+				liste[x] = tempListe[i];
+			}
+			x++;
+		}
+
 	}
 
 }
